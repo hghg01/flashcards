7ff3e27e145aa0a7abffd276ea74097876eb3eb0
hghg01 <102838081+hghg01@users.noreply.github.com>
2024-09-24 07:49:20 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,104 @@
+Flashcards
+
+criaCartao (
+'Biologia',
+'Relações ecológicas  - Como são classificadas com base nos benefícios envolvidos?',
+'Harmônicas e desarmônicas')
+
+criaCartao (
+'Biologia',
+'Cloroplastos - Responsáveis pelo processo de...',
+'Fotossíntese nos vegetais.')
+
+criaCartao(
+'Biologia',
+'Vitamina B9 - Avitaminose gera...’,
+'Anemia megaloblástica')
+
+criaCartao(
+'Biologia',
+'Principais funções do sistema respiratório? (4)’,
+'1. Garantir as trocas gasosas com o meio ambiente; 2. Regular a temperatura corpórea; 3. Regular pH do sangue; 4. Liberação de água.')
+
+criaCartao(
+'Biologia ',
+'Vírus - Como é feita sua reprodução?',
+'Pela injeção de seu material genético no interior da célula alvo.')
+
+criaCartao(
+'História',
+'Bloqueio continental - Objetivo? Era Napoleônica ',
+'Afetar a economia inglesa por intermédio do comércio.')
+
+criaCartao (
+'História ',
+'Pacto colonial - O que estabelecia?',
+'A colônia só podia fazer comércio com a Metrópole.')
+
+criaCartao (
+'História ',
+'Revolução francesa - Principais causas? (3)',
+'1. Impopularidade do governo de Bourbons; 2. ↑Número de guerras; 3. Busca por poder político da burguesia')
+
+criaCartao (
+'História ',
+‘Império napoleônico - Período histórico?’,
+‘Entre 1804 e 1815.’)
+
+criaCartao (
+‘História’,
+‘Era Vargas
+Momento histórico?’,
+‘Entre 1930 e 1945.’)
+
+criaCartao(
+‘Física ‘,
+‘Dinâmica
+O "equilíbrio" pode ser....’,
+‘Estático ou dinâmico.’)
+
+criaCartao (
+'Física’,
+'Capacidade térmica - Fórmula?',
+'C = m.c = Onde: C - capacidade térmica; m - massa; c - calor específico.')
+
+criaCartao (
+'Física ',
+'Ondas estacionárias - De que resultam?',
+'Superposição de ondas periódicas e iguais.')
+
+criaCartao (
+'Física ',
+'O que define a formação de sombra ou penumbra é o...',
+'tipo de fonte de luz, se pontual ou extensa')
+
+criaCartao(
+'Física',
+'Qual a carga elementar de um elétron?',
+'1,6 x 10-19 C.')
+
+criaCartao (
+'Química',
+'Carbono - Propriedades fundamentais?',
+'1. Tetracovalência constante; 2. Nox variável de -4 a +4; 3. Formação de cadeias; 4. Anfótero.')
+
+criaCartao (
+
+'Química ',
+'Como se classificam as ligações entre átomos de carbono?',
+'sigma ou pi')
+
+criaCartao (
+'Química ',
+'Elementos ametais - Características?',
+'1. Má condutibilidade; 2. Não maleáveis; 3. Não dúcteis; 4. Não possuem brilho.')
+
+criaCartao (
+'Química ',
+'Processos exotérmicos - Definição?',
+'Processos em que ocorre a liberação de calor.')
+
+criaCartao(
+'Química',
+'Variação de entalpia - Fórmula?',
+'ΔH = Hprodutos – Hreagentes')
